1c698dff04786de9d13994b0da1b42a411a229c1
Admin <gregouze>
2017-02-03 17:00:02 +0100

    meh

--- package.sh
+++ package.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
-rm -rf pak.zip
-zip -r pak.zip bin/simplify js/index.js js/stl2stl.js js/stl2obj.js js/node_modules
+rm -rf pak pak.zip
+mkdir pak
+#cp bin/assimp pak/
+cp bin/simplify pak/
+cp js/index.js pak/
+cp js/stl2stl.js pak/
+cp js/stl2obj.js pak/
+cp -r js/node_modules pak/
+cd pak
+zip -r pak.zip *
+cp pak.zip ../
+cd ..
+rm -rf pak
